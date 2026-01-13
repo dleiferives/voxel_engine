24f4a77366d6b3c4df26bd47867b0ecc7fbbc699
Dylan Leifer-Ives <dleiferives@gmail.com>
2026-01-13 10:52:27 -0800

    Solve vulkan finding and building
    Closes #2
    However, it would be good to get the modules up and running even though
    they are not at the moment. So that could be a thing to handle later.
    
    Depends on the user having installed Vulkan on their own so that it can
    be found. Should leave a note in the Readme regarding this.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 
-#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+#include <vulkan/vulkan.hpp>
 
 
 const char vertexShaderSource[] = {
@@ -17,147 +18,6 @@ const char fragmentShaderSource[] = {
 #embed "shaders/cube.frag"
     ,0
 };
-
-
-// Camera
-typedef struct {
-    glm::vec3 pos;
-    glm::vec3 front;
-    glm::vec3 up;
-    float yaw;
-    float pitch;
-    float sensitivity;
-    float speed;
-} Camera;
-
-typedef struct {
-    float lastX;
-    float lastY;
-    float delta_time;
-    float last_frame;
-    Camera camera;
-} Manager;
-
-// Settings
-const unsigned int SCR_WIDTH = 1280;
-const unsigned int SCR_HEIGHT = 720;
-
-// Globals
-bool first_mouse = true;
-Manager g_manager = {
-    .lastX = SCR_WIDTH / 2.0,
-    .lastY = SCR_HEIGHT / 2.0,
-    .delta_time = 0.0,
-    .last_frame = 0.0,
-    .camera = {
-        .pos   = glm::vec3(0.0f, 0.0f, 3.0f),
-        .front = glm::vec3(0.0f, 0.0f, -1.0f),
-        .up    = glm::vec3(0.0f, 1.0f, 0.0f),
-        .yaw   = -90.0f,
-        .pitch = 0.0f,
-        .sensitivity = 0.1f,
-        .speed= 2.5f
-    }
-};
-
-
-float g_cube[] = {
-    -0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f, -0.5f,
-    0.5f,  0.5f, -0.5f,
-    0.5f,  0.5f, -0.5f,
-    -0.5f,  0.5f, -0.5f,
-    -0.5f, -0.5f, -0.5f,
-
-    -0.5f, -0.5f,  0.5f,
-    0.5f, -0.5f,  0.5f,
-    0.5f,  0.5f,  0.5f,
-    0.5f,  0.5f,  0.5f,
-    -0.5f,  0.5f,  0.5f,
-    -0.5f, -0.5f,  0.5f,
-
-    -0.5f,  0.5f,  0.5f,
-    -0.5f,  0.5f, -0.5f,
-    -0.5f, -0.5f, -0.5f,
-    -0.5f, -0.5f, -0.5f,
-    -0.5f, -0.5f,  0.5f,
-    -0.5f,  0.5f,  0.5f,
-
-    0.5f,  0.5f,  0.5f,
-    0.5f,  0.5f, -0.5f,
-    0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f,  0.5f,
-    0.5f,  0.5f,  0.5f,
-
-    -0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f,  0.5f,
-    0.5f, -0.5f,  0.5f,
-    -0.5f, -0.5f,  0.5f,
-    -0.5f, -0.5f, -0.5f,
-
-    -0.5f,  0.5f, -0.5f,
-    0.5f,  0.5f, -0.5f,
-    0.5f,  0.5f,  0.5f,
-    0.5f,  0.5f,  0.5f,
-    -0.5f,  0.5f,  0.5f,
-    -0.5f,  0.5f, -0.5f,
-};
-
-
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-
-void mouse_callback(GLFWwindow* window, double x_in, double y_in){
-   float x = static_cast<float>(x_in);
-   float y = static_cast<float>(y_in);
-
-   if (first_mouse){
-       g_manager.lastX = x;
-       g_manager.lastY = y;
-       first_mouse = false;
-   }
-
-   // Update our last positions
-   float delta_x = (x - g_manager.lastX) * g_manager.camera.sensitivity;
-   float delta_y = (g_manager.lastY - y) * g_manager.camera.sensitivity;
-   g_manager.lastX = x;
-   g_manager.lastY = y;
-
-   g_manager.camera.yaw += delta_x;
-   g_manager.camera.pitch += delta_y;
-
-   // Compute camera changes
-   g_manager.camera.pitch = (g_manager.camera.pitch > 89.0f) ? 89.0f : g_manager.camera.pitch;
-   g_manager.camera.pitch = (g_manager.camera.pitch < -89.0f) ? -89.0f : g_manager.camera.pitch;
-   glm::vec3 front;
-   front.x = cos(glm::radians(g_manager.camera.yaw)) * cos(glm::radians(g_manager.camera.pitch));
-   front.y = sin(glm::radians(g_manager.camera.pitch));
-   front.z = sin(glm::radians(g_manager.camera.yaw)) * cos(glm::radians(g_manager.camera.pitch));
-   g_manager.camera.front = glm::normalize(front);
-}
-
-
-void processInput(GLFWwindow* window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-
-    float cameraSpeed = static_cast<float>(g_manager.camera.speed * g_manager.delta_time);
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        g_manager.camera.pos += cameraSpeed * g_manager.camera.front;
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        g_manager.camera.pos -= cameraSpeed * g_manager.camera.front;
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        g_manager.camera.pos -= glm::normalize(glm::cross(g_manager.camera.front, g_manager.camera.up)) * cameraSpeed;
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        g_manager.camera.pos += glm::normalize(glm::cross(g_manager.camera.front, g_manager.camera.up)) * cameraSpeed;
-}
-
 
 
 
@@ -173,7 +33,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "C++23 OpenGL Window", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Voxel Engine Window", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
@@ -181,18 +41,7 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD\n";
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return 1;
-    }
-
-    glEnable(GL_DEPTH_TEST);
 
     // Setup our offset buffers thing
     std::vector<glm::vec3> cube_positions;
@@ -206,88 +55,12 @@ int main()
     }
 
     // Compile Shaders
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    const char *vptr = vertexShaderSource;
-    glShaderSource(vertexShader, 1, &vptr, NULL);
-    glCompileShader(vertexShader);
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    const char *fptr = fragmentShaderSource;
-    glShaderSource(fragmentShader, 1, &fptr, NULL);
-    glCompileShader(fragmentShader);
-
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // Setup buffers
-    //
-    unsigned int VBO, VAO, cubesVBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &cubesVBO);
-
-    // cube
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_cube), g_cube, GL_STATIC_DRAW);
-
-    // Position attribute
-    // Stride could be 3 * sizeof(float). But since we're tightly packing I will just do that explicitly
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // cubes positions
-    glBindBuffer(GL_ARRAY_BUFFER, cubesVBO);
-    glBufferData(
-        GL_ARRAY_BUFFER,
-        cube_positions.size() * sizeof(glm::vec3),
-        cube_positions.data(),
-        GL_STATIC_DRAW
-    );
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(1);
-
-    // We are changing per instance not per vertex
-    glVertexAttribDivisor(1, 1);
 
 
     while (!glfwWindowShouldClose(window)) {
-        float currentFrame = static_cast<float>(glfwGetTime());
-        g_manager.delta_time = currentFrame - g_manager.last_frame;
-        g_manager.last_frame = currentFrame;
-
-        processInput(window);
-
-        glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-
-        // Transformation matrices
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = glm::lookAt(g_manager.camera.pos, g_manager.camera.pos + g_manager.camera.front, g_manager.camera.up);
-        glm::mat4 model = glm::mat4(1.0f);
-        // model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
-
-        glBindVertexArray(VAO);
-        glDrawArraysInstanced(GL_TRIANGLES, 0, 36, (GLsizei)cube_positions.size());
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &cubesVBO);
-    glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
     glfwTerminate();
